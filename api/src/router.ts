a4d298f828147dab9f5fbafde84e68f88a90ae97
Rafael Vieira Barbosa <rafaelviebar@gmail.com>
2021-05-21 08:40:18 -0300

    feat: add AswearController

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AnswearController } from "./controllers/AnswerController";
 import { SendMailController } from "./controllers/SendMailController";
 import { SurveyController } from "./controllers/SurveyController";
 import { UserController } from "./controllers/UserController";
@@ -8,6 +9,7 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
+const answearController = new AnswearController();
 
 router.post('/users', userController.create);
 
@@ -15,5 +17,7 @@ router.get('/surveys', surveyController.show);
 router.post('/surveys', surveyController.create);
 
 router.post('/sendMail', sendMailController.execute);
+
+router.get('/answers/:value', answearController.execute);
 
 export { router };
