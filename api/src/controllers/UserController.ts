4778e087c035071ae89684617db36af2ed6dd76a
Rafael Vieira Barbosa <rafaelviebar@gmail.com>
2021-05-22 21:22:03 -0300

    refactor: removing test' db on the tests

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UserRepository } from "../repositories/UserRepository";
+import * as yup from 'yup';
 
 class UserController {
   async create(request:Request, response:Response) {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required()
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err});
+    }
+
     const { name, email } = request.body;
 
     const userRepository = getCustomRepository(UserRepository);
